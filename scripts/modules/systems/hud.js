2a8c45c99493d378cdc3659f188640e7d9f7f89a
makc <makc.the.great@gmail.com>
2014-10-18 19:03:38 +0200

    support touch screens

--- scripts/modules/systems/hud.js
+++ scripts/modules/systems/hud.js
@@ -4,6 +4,11 @@ define(["ecs", "components"], function (ecs, components) {
 		return number.toString().replace(/1/g, "I").replace(/0/g, "O");
 	};
 
+	// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/touchevents.js
+	if (('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
+		document.getElementById("instructions").innerHTML = "Touch on the right side and drag to move, touch on the left to shoot.";
+	}
+
 	return { update: function(dt) {
 		ecs.for_each([components.Hero], function(player) {
 			var hero = player.get(components.Hero);
--- scripts/modules/systems/playerControls.js
+++ scripts/modules/systems/playerControls.js
@@ -22,15 +22,66 @@ define(["ecs", "components"], function (ecs, components) {
 	var forward = new THREE.Vector3();
 	var sideways = new THREE.Vector3();
 
+	// alternative controls for touch screens
+	// inspired by https://github.com/sebleedelisle/JSTouchController/blob/master/TouchControl.html
+
+	var firstTouch = -1, otherTouches = 0;
+	var firstTouchOrigin = new THREE.Vector2();
+	var firstTouchVector = new THREE.Vector2();
+
+	document.addEventListener ('touchstart', function (e) {
+		for (var i = 0; i < e.changedTouches.length; i++) {
+			var touch = e.changedTouches[i];
+			if ((firstTouch < 0) && (touch.clientX > 0.5 * window.innerWidth)) {
+				firstTouch = touch.identifier;
+				firstTouchOrigin.set (touch.clientX, touch.clientY);
+			} else {
+				otherTouches++;
+			}
+		}
+	});
+	document.addEventListener ('touchmove', function (e) {
+		e.preventDefault ();
+		for (var i = 0; i < e.changedTouches.length; i++) {
+			var touch = e.changedTouches[i];
+			if (touch.identifier == firstTouch) {
+				firstTouchVector.set (touch.clientX, touch.clientY).sub (firstTouchOrigin);
+				break;
+			}
+		}
+	});
+	document.addEventListener ('touchend', function (e) {
+		for (var i = 0; i < e.changedTouches.length; i++) {
+			var touch = e.changedTouches[i];
+			if (touch.identifier == firstTouch) {
+				firstTouch = -1;
+				firstTouchVector.set (0, 0);
+			} else {
+				otherTouches--;
+			}
+		}
+	});
 
 	return { update: function (dt) {
 		ecs.for_each([components.Hero, components.Motion], function(player) {
 			var motion = player.get(components.Motion);
 			if(!motion.airborne) {
 
+				// interpret touch vector
+				var rotationTouchInput = firstTouchVector.x;
+				if (rotationTouchInput > 0) rotationTouchInput = Math.max (0, rotationTouchInput - 0.02 * window.innerWidth);
+				if (rotationTouchInput < 0) rotationTouchInput = Math.min (0, rotationTouchInput + 0.02 * window.innerWidth);
+				rotationTouchInput = Math.max (-1, Math.min (1, rotationTouchInput / (0.2 * window.innerWidth)));
+
+				var movementTouchInput = firstTouchVector.y;
+				movementTouchInput = Math.max (-1, Math.min (1, movementTouchInput / (0.1 * window.innerWidth)));
+
+				// there is no easy way to look up or down using touch screen only
+				var lookDownTouchInput = (Math.abs (rotationTouchInput) + Math.abs (movementTouchInput) > 0) ? 1 : 0; 
+
 				// look around
-				var sx = keysPressed[keys.UP] ? 0.04 : (keysPressed[keys.DN] ? -0.04 : 0);
-				var sy = keysPressed[keys.LT] ? 0.04 : (keysPressed[keys.RT] ? -0.04 : 0);
+				var sx = (keysPressed[keys.UP] ? 0.04 : (keysPressed[keys.DN] ? -0.04 : 0)) - 0.04 * lookDownTouchInput;
+				var sy = (keysPressed[keys.LT] ? 0.04 : (keysPressed[keys.RT] ? -0.04 : 0)) - 0.04 * rotationTouchInput;
 
 				if(Math.abs(sx) >= Math.abs(motion.spinning.x)) motion.spinning.x = sx;
 				if(Math.abs(sy) >= Math.abs(motion.spinning.y)) motion.spinning.y = sy;
@@ -41,7 +92,7 @@ define(["ecs", "components"], function (ecs, components) {
 				// calculate sideways direction from forward direction
 				sideways.set(forward.z, 0, -forward.x);
 
-				forward.multiplyScalar(keysPressed[keys.W] ? -0.1 : (keysPressed[keys.S] ? 0.1 : 0));
+				forward.multiplyScalar((keysPressed[keys.W] ? -0.1 : (keysPressed[keys.S] ? 0.1 : 0)) + 0.1 * movementTouchInput);
 				sideways.multiplyScalar(keysPressed[keys.A] ? -0.1 : (keysPressed[keys.D] ? 0.1 : 0));
 
 				var combined = forward.add(sideways);
@@ -56,7 +107,7 @@ define(["ecs", "components"], function (ecs, components) {
 			if(shotgun) {
 
 				// ...try to use it
-				shotgun.pullingTrigger = keysPressed[keys.SP];
+				shotgun.pullingTrigger = keysPressed[keys.SP] || (otherTouches > 0);
 			}
 
 			return true;
